c8e27b8e776db4cd9e3c110b8f02bb35d28d2ee1
sabrinaschimidt <sabrinargschimidt@hotmail.com>
2023-11-20 23:05:24 -0400

    inclusão validação cruzada

--- LeArquivos.py
+++ LeArquivos.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from glob import glob
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.impute import SimpleImputer
@@ -43,31 +45,25 @@ if not dados_combinados.empty:
     # Divide os dados em conjuntos de treinamento e teste
     X_train, X_test, y_train, y_test = train_test_split(X_encoded, y_encoded, test_size=0.2, random_state=42)
 
+    # Aplicar k-fold para avaliação de desempenho
+    k_fold = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
     # 1. K Nearest Neighbors (KNN)
     knn_classifier = KNeighborsClassifier(n_neighbors=3)
-    knn_classifier.fit(X_train, y_train)
-    knn_predictions = knn_classifier.predict(X_test)
+    knn_accuracy_scores = cross_val_score(knn_classifier, X_encoded, y_encoded, cv=k_fold, scoring='accuracy')
+    print(f"Acurácia média do KNN: {knn_accuracy_scores.mean():.2f}")
 
-    # Avalie a precisão do modelo KNN
-    knn_accuracy = accuracy_score(y_test, knn_predictions)
-    print(f"Acurácia do KNN: {knn_accuracy:.2f}")
-
-  # 2. Naive Bayes
+    # 2. Naive Bayes
+    one_hot_encoder = OneHotEncoder(drop='first', sparse=False)
+    X_one_hot_encoded = one_hot_encoder.fit_transform(X)
+    
     naive_bayes_classifier = GaussianNB()
-    naive_bayes_classifier.fit(X_train, y_train)
-    naive_bayes_predictions = naive_bayes_classifier.predict(X_test)
-
-    # Avalie a precisão do modelo Naive Bayes
-    naive_bayes_accuracy = accuracy_score(y_test, naive_bayes_predictions)
-    print(f"Acurácia do Naive Bayes: {naive_bayes_accuracy:.2f}")
+    naive_bayes_accuracy_scores = cross_val_score(naive_bayes_classifier, X_one_hot_encoded, y_encoded, cv=k_fold, scoring='accuracy')
+    print(f"Acurácia média do Naive Bayes: {naive_bayes_accuracy_scores.mean():.2f}")
 
     # 3. Multilayer Perceptron (MLP)
     mlp_classifier = MLPClassifier(hidden_layer_sizes=(100,), max_iter=500)
-    mlp_classifier.fit(X_train, y_train)
-    mlp_predictions = mlp_classifier.predict(X_test)
-
-    # Avalie a precisão do modelo MLP
-    mlp_accuracy = accuracy_score(y_test, mlp_predictions)
-    print(f"Acurácia do MLP: {mlp_accuracy:.2f}")
+    mlp_accuracy_scores = cross_val_score(mlp_classifier, X_encoded, y_encoded, cv=k_fold, scoring='accuracy')
+    print(f"Acurácia média do MLP: {mlp_accuracy_scores.mean():.2f}")
 else:
     print("Atenção: Nenhum dado foi carregado. Verifique se os arquivos CSV estão corretamente especificados.")
